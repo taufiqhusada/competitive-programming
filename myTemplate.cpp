eedcb4fd413dd55a432193ac42eaef02d3996d0e
taufiqhusada <taufiqhusdr@gmail.com>
2018-12-29 21:36:41 +0700

    simple template

--- myTemplate.cpp
+++ myTemplate.cpp
@@ -1,19 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long LL;
-const LL mod = 998244353;
-#define maxn 120000
-LL f[4][maxn], a[maxn];
-char s[maxn];
+
+#define REP(i,n)      for ( int i = 0, _n = (n); i < _n; i++ )
+#define FOR(i,a,b)    for ( int i = (a), _n = (b); i <= _n; i++ )
+#define FORD(i,a,b)   for ( int i = (a), _n = (b); i >= _n; i-- )
+#define RESET(a,val)  memset(a,val,sizeof(a));
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define INF 1e9
+#define MOD 1000000007
+
+typedef vector<int> vi;
+typedef vector<pair<int,int> > vii;
+typedef long long ll;
+typedef unsigned long long ull;
+
 int main(){
-	int n;
-	cin >> n >> s + 1;
-	for(int i = 1; i <= n; i += 1) cin >> a[i];
-	for(int i = 1; i <= n; i += 1){
-		f[0][i] = f[0][i - 1] + a[i] * (s[i] == 'h');
-		f[1][i] = min(f[0][i], f[1][i - 1] + a[i] * (s[i] == 'a'));
-		f[2][i] = min(f[1][i], f[2][i - 1] + a[i] * (s[i] == 'r'));
-		f[3][i] = min(f[2][i], f[3][i - 1] + a[i] * (s[i] == 'd'));
-	}
-	cout << min(min(f[0][n], f[1][n]), min(f[2][n], f[3][n]));
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
 }
