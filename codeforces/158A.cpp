a509941cb8103944c63c1a40617068629fdf9b8f
taufiqhusada <taufiqhusdr@gmail.com>
2019-01-05 09:17:19 +0700

    implementation

---
+++ codeforces/158A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define REP(i,n)      for ( int i = 0, _n = (n); i < _n; i++ )
+#define FOR(i,a,b)    for ( int i = (a), _n = (b); i <= _n; i++ )
+#define FORD(i,a,b)   for ( int i = (a), _n = (b); i >= _n; i-- )
+#define RESET(a,val)  memset(a,val,sizeof(a));
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define INF 1e9
+#define MOD 1000000007
+
+typedef vector<int> vi;
+typedef vector<pair<int,int> > vii;
+typedef long long ll;
+typedef unsigned long long ull;
+
+int n,k,a[55];
+int ans;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin>>n>>k;
+    REP(i,n) cin>>a[i];
+    int batas = a[k-1];
+    REP(i,n){
+        if (a[i]>=batas && a[i]>0) ans++;
+    }
+    cout<<ans<<endl;
+}
