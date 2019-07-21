b57c8d49b46ebc6279515a1a58589c2507e39dbb
taufiqhusada <taufiqhusdr@gmail.com>
2019-07-21 21:02:58 +0700

    dsu/vector

--- codeforces/1131F.cpp
+++ codeforces/1131F.cpp
@@ -17,12 +17,14 @@ typedef vector<pair<int,int> > vii;
 typedef long long ll;
 typedef unsigned long long ull;
 
-int dsu[150000],sz[150000],done[150000];
+int dsu[150005],sz[150005];
+std::vector<vi> v(150005);
 
 void init(int n){
     FOR(i,1,n){
         dsu[i] = i;
         sz[i] = 1;
+        v[i].pb(i);
     }
 }
 
@@ -36,14 +38,19 @@ int root(int u){
 void join(int a, int b){
     int root_A = root(a);
     int root_B = root(b);
-    if(size[root_A] < size[root_B ]){
-        dsu[ root_A ] = dsu[root_B];
+    if(sz[root_A] < sz[root_B ]){
+        dsu[root_A] = dsu[root_B];
         sz[root_B] += sz[root_A];
+        for (auto i: v[root_A]) v[root_B].pb(i);
+        v[root_A].clear();
     }
     else{
         dsu[ root_B ] = dsu[root_A];
         sz[root_A] += sz[root_B];
+        for (auto i: v[root_B]) v[root_A].pb(i);
+        v[root_B].clear();
     }
+}
 
 bool find(int a, int b){
     return root(a)==root(b);
@@ -55,9 +62,13 @@ int main(){
     ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     cin>>n;
     init(n);
-    REP(i,n-1){
+    REP(i,n-2){
         cin>>a>>b;
         join(a,b);
-                }
-
+    }
+    cin>>a>>b;
+    int grandA = root(a);
+    int grandB = root(b);
+    for (auto i: v[grandA]) cout<<i<<" ";
+    for (auto i: v[grandB]) cout<<i<<" ";
 }
